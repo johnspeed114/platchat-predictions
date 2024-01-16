63eebd943ce25263f18896656894687694ce076b
John Law <johnlaw114@gmail.com>
2024-01-16 16:55:46 -0500

    vercel analytics and speed insights added

--- app/components/FilterSelector.tsx
+++ app/components/FilterSelector.tsx
@@ -19,17 +19,20 @@ export default function FilterSelector({ type, setType }: FilterSelectorProps) {
 
   return (
     <div className="flex flex-col items-center justify-center w-full mb-6 sm:mb-10">
-      <select
-        value={type}
-        onChange={handleFilterChange}
-        className="p-2 border border-gray-300 rounded"
-      >
-        <option value="Total">Total</option>
-        <option value="2023">2023</option>
-        <option value="2022">2022</option>
-        <option value="2021">2021</option>
-        <option value="Game Changers">Game Changers</option>
-      </select>
+      <label htmlFor="filterSelector">
+        <select
+          value={type}
+          id="filterSelector"
+          onChange={handleFilterChange}
+          className="p-2 border border-gray-300 rounded"
+        >
+          <option value="Total">Total</option>
+          <option value="2023">2023</option>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="Game Changers">Game Changers</option>
+        </select>
+      </label>
     </div>
   );
 }
